7066c522caded90a2df09149e67ba6383d8d5133
mehmetuludag <mehmetuludag96@gmail.com>
2022-06-23 16:05:23 +0300

    file name changed

---
+++ src/test/java/challenge/users/UsersRunner.java
@@ -0,0 +1,12 @@
+package challenge.users;
+
+import com.intuit.karate.junit5.Karate;
+
+class UsersRunner {
+    
+    @Karate.Test
+    Karate testUsers() {
+        return Karate.run("ui").relativeTo(getClass());
+    }    
+
+}
